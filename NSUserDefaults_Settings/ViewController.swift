484e699ed24a0249aecaeb4fbe7d002328766825
aakash <aakash.jain@soprasteria.com>
2021-04-26 17:42:49 +0530

    save and read data.

--- NSUserDefaults_Settings/ViewController.swift
+++ NSUserDefaults_Settings/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDateLabel()
+        
+        
+    }
+    
+    func updateDateLabel() {
+        let lastUpdateDate = UserDefaults.standard.object(forKey: "buttonTapped") as? NSDate
+        
+        if let hasSavedDate = lastUpdateDate {
+            dateLabel.text = hasSavedDate.description
+        } else {
+            dateLabel.text = "No date saved."
+        }
     }
 
-
+    @IBAction func onDateBtnTap(_ sender: UIButton) {
+        let now = NSDate()
+        UserDefaults.standard.set(now, forKey: "buttonTapped");
+        updateDateLabel()
+    }
+    
 }
 
