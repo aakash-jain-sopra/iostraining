11994bc01dea791d93112a57e5e1a935fa6749fe
aakash <aakash.jain@soprasteria.com>
2021-04-26 17:44:21 +0530

    segues training

--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let d = segue.destination
+    }
 
 }
 
